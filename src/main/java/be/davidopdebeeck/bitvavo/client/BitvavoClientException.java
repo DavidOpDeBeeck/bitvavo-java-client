0a4b2254369aae48cc31fa1ecc7082a628bb3942
David Op de Beeck <davidopdebeeck@outlook.be>
2022-05-28 11:11:56 +0200

    add reminder to clean up existing RuntimeException usages

--- src/main/java/be/davidopdebeeck/bitvavo/client/BitvavoClientException.java
+++ src/main/java/be/davidopdebeeck/bitvavo/client/BitvavoClientException.java
@@ -1,5 +1,6 @@
 package be.davidopdebeeck.bitvavo.client;
 
+// TODO: replace RuntimeException with BitvavoClientException when appropriate
 public class BitvavoClientException extends RuntimeException {
 
     public BitvavoClientException(Throwable cause) {
